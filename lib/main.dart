5fcba6af2d1abf674ada4c0efcec204aeb699b44
૮₍ ´• ˕ •` ₎ა <167632296+strawpies@users.noreply.github.com>
2026-04-20 19:38:20 -0300

    Initialize Flutter app with custom theme and home screen
    Initial setup of Flutter application with theme and home screen.

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'criacao_fofoca_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Projeto Fofoca',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        fontFamily: 'ArcadeFont',
+
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Colors.black),
+            borderRadius: BorderRadius.zero,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Colors.purple),
+            borderRadius: BorderRadius.zero,
+          ),
+          labelStyle: TextStyle(color: Colors.black, fontSize: 14),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.yellow,
+            foregroundColor: Colors.black,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            side: BorderSide(width: 3, color: Colors.black),
+            elevation: 5,
+          ),
+        ),
+      ),
+      home: const CriacaoFofocaScreen(),
+    );
+  }
+}
